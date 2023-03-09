5dfa58d63777eed24cff1498656ac6e2b49a22c0
Fernando Caye <fernando.caye@cellid.com>
2023-03-09 10:35:50 +0900

    Sorting faces of mesh by average depth.

--- src/main.c
+++ src/main.c
@@ -195,15 +195,39 @@ void update(void)
             projected_points[j].y += window_height / 2;
         }
 
+        // Calculate the average depth for each face based on the vertices
+        // z value after transformations
+        float avg_depth = (float)(transformed_vertices[0].z +
+                                  transformed_vertices[1].z +
+                                  transformed_vertices[2].z) /
+                          3;
+
         triangle_t projected_triangle = {
             .points = {
                 {projected_points[0].x, projected_points[0].y},
                 {projected_points[1].x, projected_points[1].y},
                 {projected_points[2].x, projected_points[2].y}},
-            .color = mesh_face.color};
+            .color = mesh_face.color,
+            .avg_depth = avg_depth};
 
         // Save the projected triangle in array of screen space triangles
         array_push(triangles_to_render, projected_triangle);
+    }
+
+    // Sort triangles to render by their avg_depth
+    int num_triangles = array_length(triangles_to_render);
+    for (int i = 0; i < num_triangles; i++)
+    {
+        for (int j = i; j < num_triangles; j++)
+        {
+            if (triangles_to_render[i].avg_depth < triangles_to_render[j].avg_depth)
+            {
+                // Swap triangle positions in array.
+                triangle_t temp = triangles_to_render[i];
+                triangles_to_render[i] = triangles_to_render[j];
+                triangles_to_render[j] = temp;
+            }
+        }
     }
 }
 
