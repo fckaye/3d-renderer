18355ff12fc6a5fdce27adb61d1d59e83a279c6a
Fernando Caye <fdcaye@gmail.com>
2023-04-20 11:22:29 +0900

    add fix to prevent accessing invalid texture index

--- src/triangle.c
+++ src/triangle.c
@@ -211,8 +211,8 @@ void draw_texel(
     interpolated_v /= interpolated_reciprocal_w;
 
     // Map the UV coordinate to the full texture width and height
-    int tex_x = abs((int)(interpolated_u * texture_width));
-    int tex_y = abs((int)(interpolated_v * texture_height));
+    int tex_x = abs((int)(interpolated_u * texture_width)) % texture_width;
+    int tex_y = abs((int)(interpolated_v * texture_height)) % texture_height;
 
     draw_pixel(x, y, texture[(tex_y * texture_width) + tex_x]);
 }
